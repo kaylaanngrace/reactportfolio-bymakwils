060128983b89818c85edd447694e14b9e0671109
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-05-21 16:25:48 -0500

    navigation component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg'
+import React from "react";
+// import profilePic from '../../assets/images/';
 
 function About() {
-    return (
-        <section className="my-5">
-            <h1 id="about">Who am I?</h1>
-            <img src={coverImage} className="my-2" style={{ width: "100/%" }} alt="cover" />
-        </section>
-    );
+  return (
+    <div>
+      <p className="">Aspiring Full Stack Developer</p>
+      {/* <img className="" src={profilePic} alt="Makayla Wilson"/> */}
+      <p className="">
+        I am Makayla. I am a new developer studying FullStack development. 
+      </p>
+      <p className=""> 
+            I'm an ambitious young adult participating in the University of Minnesota Coding Bootcamp. 
+            I am also a hardworking individual with 5+ years of customer service experience. 
+            Proven to be adept in independent and collaborative environments.
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
-function Nav() {
-
-    return (
-        <header>
-            <h2>
-                <a href="/">
-                   FullStack<span role="img" aria-label="computer"> 👩🏽‍💻 </span> 
-                </a>
-            </h2>
-        
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
-                            About Me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+function Navigation(props) {
+  const navs = ["About", "Portfolio", "Contact", "Resume"];
+  return (
+    <div className="">
+      <ul className="">
+        {navs.map((nav) => (
+          <li
+            className={
+              props.currentPage === nav
+            }
+            key={nav}>
+            <a
+              href={"#" + nav.toLowerCase()}
+              onClick={() => props.handlePageChange(nav)}
+              className={
+                props.currentPage === nav
+              }>
+              {nav}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Nav;
+export default Navigation;
