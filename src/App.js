5caccdbf8b9e7e1c5b2656153a9fed3301bcb939
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-05-21 16:31:55 -0500

    added contact component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Nav from './components/Navigation/Navigation';
 import About from './components/About/About';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Nav></Nav>
+      <Header>
+        <Nav></Nav>
+      </Header>
       <main>
         <About></About>
+
+        <Portfolio></Portfolio>
+
       </main>
+      <Footer></Footer>
     </div>
   );
 }
